1aa284bc196a058c650b19a788dc6084a3587cde
Hamzah <82611798+brobro10000@users.noreply.github.com>
2021-10-05 13:58:14 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -26,12 +26,12 @@ async function start() {
 }
 start();
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../client/build")));
-// }
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-// });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 
 db.once("open", () => {
